3252b9d7d1f242671e885101afa6faf051fa4ad5
quietpay-builder <builder@example.com>
2026-09-09 11:07:47 +0100

    Witness-derived admin and contract index

---
+++ contracts/src/index.ts
@@ -0,0 +1,29 @@
+// This file is part of midnightntwrk/example-quietpay.
+// Copyright (C) Midnight Foundation
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { CompiledContract } from "@midnight-ntwrk/midnight-js-protocol/compact-js";
+
+export * from "./managed/quietpay/contract/index.js";
+export * from "./witnesses";
+
+import * as CompiledQuietPayContract from "./managed/quietpay/contract/index.js";
+import * as Witnesses from "./witnesses";
+
+export const CompiledQuietPayContractContract = CompiledContract.make<
+  CompiledQuietPayContract.Contract<Witnesses.QuietPayPrivateState>
+>("QuietPay", CompiledQuietPayContract.Contract<Witnesses.QuietPayPrivateState>).pipe(
+  CompiledContract.withWitnesses(Witnesses.witnesses),
+  CompiledContract.withCompiledFileAssets("./managed/quietpay"),
+);
